467a89b780bbfc8c2ef5ca427d96e6a912e3e9e7
Katarzyna Herman <katarzyna.herman@ensteam.com>
2017-01-03 20:11:10 +0100

    +cleaning

--- server/src/main/scala/controller/Server.scala
+++ server/src/main/scala/controller/Server.scala
@@ -26,7 +26,7 @@ class Server extends Actor {
 	import Tcp._
 	import context.system
 
-	IO(Tcp) ! Bind(self, new InetSocketAddress("localhost", Properities.PORT))
+	IO(Tcp) ! Bind(self, new InetSocketAddress(Properities.PATH, Properities.PORT))
 
 	def receive = {
 		case b@Bound(localAddress) =>
--- shared/src/main/scala/model/Properities.scala
+++ shared/src/main/scala/model/Properities.scala
@@ -4,7 +4,7 @@ package model
 	* Created by kass on 19.10.16.
 	*/
 object Properities {
-	val PATH = "localhost"
+	val PATH = "192.168.1.4"
 	val PORT = 19999
 	val DELIMITER = "/end/"
 
